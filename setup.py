6d55c849f86f0b8ad098e52f866d7ab1a34501c8
scwatts <s.watts2@student.unimelb.edu.au>
2018-02-14 13:07:30 +1100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 import setuptools
+import subprocess
+import shutil
 import sys
-
-
-# Check that Python 3.5+ is installed
-if sys.version_info < (3,5):
-    msg = 'ERROR: %s requires Python 3.5+. Python %d.%d detected'
-    print(msg % (PACKAGE_NAME, *sys.version_info[:2]))
-    sys.exit(1)
 
 
 # Set package details
@@ -17,15 +12,33 @@ package_version = '0.0.1'
 author = 'Stephen Watts'
 licence = 'gpl'
 
+# Check that Python 3.5+ is installed
+if sys.version_info < (3,5):
+    msg = 'error: %s requires Python 3.5+. Python %d.%d detected'
+    print(msg % (package_name, *sys.version_info[:2]))
+    sys.exit(1)
+
+# Check for gfortran
+if not shutil.which('gfortran'):
+    msg = 'error: %s requires gfortran for installation'
+    print(msg % package_name)
+    sys.exit(1)
+
+# Compile hclust.f
+hclustf_src = 'src/hclust.f'
+hclustf_obj = 'src/hclustf.o'
+cmd = 'gfortran -fPIC -c %s -o %s' % (hclustf_src, hclustf_obj)
+stderr = subprocess.run(cmd, shell=True, check=True)
+
+# Set extension details
 source_files = ['src/copheneticd.c', 'src/bindings.c', 'src/dist_nodes.c',
                 'src/hclust.c', 'src/cuttree.c', 'src/hclust-utils.c']
-fortran_objects = ['src/hclustf.o']
+fortran_objects = [hclustf_obj]
 extension = setuptools.Extension(
         '_treeclust',
         source_files,
         extra_objects=fortran_objects,
         extra_compile_args=['-Wno-maybe-uninitialized'])
-
 
 # Call setup
 setuptools.setup(
